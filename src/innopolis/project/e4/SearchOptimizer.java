e8aa41adbf33341f2fa5b725424d2a5af1d9843e
KryukovaLidiya <Lidakrukova1994@gmail.com>
2016-07-18 01:25:42 +0600

    algorithm ddfs, time = 3 level, needed search and get Info.

--- src/innopolis/project/e4/SearchOptimizer.java
+++ src/innopolis/project/e4/SearchOptimizer.java
@@ -1,12 +1,16 @@
 package innopolis.project.e4;
 
 import com.sun.javaws.exceptions.InvalidArgumentException;
+import com.sun.org.apache.xpath.internal.operations.String;
 import innopolis.project.e4.models.Airport;
 import innopolis.project.e4.models.Flight;
 import innopolis.project.e4.providers.DataProvider;
 
+import java.awt.*;
 import java.util.Date;
 import java.util.List;
+import  java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by Xomak on 15.07.2016.
@@ -14,8 +18,12 @@ import java.util.List;
  */
 public class SearchOptimizer {
     enum Criterion {COST, TIME}
-
     private DataProvider provider;
+    enum Color {WIGHT, LITLEBLACK, BLACK};
+    private static ArrayList<Integer> timeIn, timeOut;
+    private static ArrayList<Integer> colorType;
+    private ArrayList<String> minPath;
+    private static int timerNow = 0;
 
     public SearchOptimizer(final DataProvider provider) {
         this.provider = provider;
@@ -24,8 +32,30 @@ public class SearchOptimizer {
     public void rebuildPathes() {
 
     }
+//curr посещенные вершины
+    public List<Flight> getPathesBetween(final Airport src,
+                                         final Airport dst,
+                                         final Date date,
+                                         final Criterion criterion) {
+    //run func dfs;
+    }
 
-    public List<Flight> getPathesBetween(final Airport src, final Airport dst, final Date date, final Criterion criterion) {
-        return null;
+    private List<Flight> dfsFlighting(Set<Airport> dest) {
+        timeIn.add(dst, this.timerNow);
+        this.timerNow++;
+        colorType.add(dst, Color.LITLEBLACK);
+        if(this.timerNow == 4) {
+            System.out.println("Sorry path was not yes");
+        }
+        //Search my dest
+        for (int i; i < minPath.size(); ++i) {
+            if (colorType[dst].equals(Color.WIGHT)) {
+                timeIn.add(dst, this.timerNow);
+                this.timerNow++;
+            }
+        }
+        PageAttributes.ColorType.add(dst, Color.BLACK);
+        timeOut.add(dst, this.timerNow);
+        this.timerNow++;
     }
 }
