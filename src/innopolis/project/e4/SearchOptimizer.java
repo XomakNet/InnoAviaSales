15f155e2593303063c797f0c3f3cb6695be338f2
KryukovaLidiya <Lidakrukova1994@gmail.com>
2016-07-19 22:14:15 +0600

    ddfs realization, I need test, and debug. This realization get only name Airport. When I will know  this path is true, i will do function for Flight in this paths.

--- src/innopolis/project/e4/SearchOptimizer.java
+++ src/innopolis/project/e4/SearchOptimizer.java
@@ -1,16 +1,11 @@
 package innopolis.project.e4;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
-import com.sun.org.apache.xpath.internal.operations.String;
 import innopolis.project.e4.models.Airport;
 import innopolis.project.e4.models.Flight;
 import innopolis.project.e4.providers.DataProvider;
 
-import java.awt.*;
-import java.util.Date;
+import java.util.*;
 import java.util.List;
-import  java.util.ArrayList;
-import java.util.Set;
 
 /**
  * Created by Xomak on 15.07.2016.
@@ -19,11 +14,9 @@ import java.util.Set;
 public class SearchOptimizer {
     enum Criterion {COST, TIME}
     private DataProvider provider;
-    enum Color {WIGHT, LITLEBLACK, BLACK};
-    private static ArrayList<Integer> timeIn, timeOut;
-    private static ArrayList<Integer> colorType;
-    private ArrayList<String> minPath;
-    private static int timerNow = 0;
+    Stack<Airport> minimalPathName = new Stack<>();
+    LinkedList<Stack<Airport>> resultFli = new LinkedList<Stack<Airport>>();
+    int MaxTrevelingCount = 5;
 
     public SearchOptimizer(final DataProvider provider) {
         this.provider = provider;
@@ -37,25 +30,38 @@ public class SearchOptimizer {
                                          final Airport dst,
                                          final Date date,
                                          final Criterion criterion) {
-    //run func dfs;
+        minimalPathName.push(src);
+        dfsFlighting(src, dst);
+        //run func dfs;
     }
 
-    private List<Flight> dfsFlighting(Set<Airport> dest) {
-        timeIn.add(dst, this.timerNow);
-        this.timerNow++;
-        colorType.add(dst, Color.LITLEBLACK);
-        if(this.timerNow == 4) {
-            System.out.println("Sorry path was not yes");
-        }
-        //Search my dest
-        for (int i; i < minPath.size(); ++i) {
-            if (colorType[dst].equals(Color.WIGHT)) {
-                timeIn.add(dst, this.timerNow);
-                this.timerNow++;
+    private void dfsFlighting(final Airport src,
+                                        final Airport dst) {
+        List<Airport> neighbours = getAirportsMates(src);
+
+        for(int i = 0; i < neighbours.size(); i++) {
+            minimalPathName.push(neighbours.get(i));
+            if(neighbours.get(i) == dst) {
+                resultFli.push(minimalPathName);
+                minimalPathName.pop();
+                break;
+            }
+            if(MaxTrevelingCount == minimalPathName.size()) {
+                if(neighbours.contains(dst)) {
+                    minimalPathName.pop();
+                    minimalPathName.push(dst);
+                    resultFli.push(minimalPathName);
+                    minimalPathName.pop();
+                    break;
+                }
+                else {
+                    minimalPathName.pop();
+                    break;
+                }
+            }
+            else{
+                dfsFlighting(neighbours.get(i), dst);
             }
         }
-        PageAttributes.ColorType.add(dst, Color.BLACK);
-        timeOut.add(dst, this.timerNow);
-        this.timerNow++;
     }
 }
