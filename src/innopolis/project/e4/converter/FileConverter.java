10fa632b47a0c2d750414279779fa19072419633
Xomak <mail@xomak.net>
2016-07-20 08:47:45 +0300

    Implemented FileConverter settings and by airline's file division (trello 20)

--- src/innopolis/project/e4/converter/FileConverter.java
+++ src/innopolis/project/e4/converter/FileConverter.java
@@ -4,15 +4,27 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Random;
+import java.util.Iterator;
 
 /**
  * Created by Xomak on 19.07.2016.
  */
 public class FileConverter {
     private BufferedReader reader = null;
+    private HashMap<String, BufferedWriter> writers = new HashMap<>();
     private BufferedWriter writer = null;
-    Random rand = new Random();
+    private String outputPath;
+    private String inputPath;
+    private int neededObjects;
+    private Random rand = new Random();
+
+    public FileConverter(String inputPath, String outputPath, int neededObjects) {
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
+        this.neededObjects = neededObjects;
+    }
 
     private Date constructDate(String yearString, String monthString, String dayOfMonthString, String timeString) {
         int year = Integer.parseInt(yearString);
@@ -21,9 +33,6 @@ public class FileConverter {
         int hours;
         int minutes;
         if(timeString.length() < 4) {
-            //hours = Integer.parseInt(timeString.substring(0, 0));
-            //System.out.println(hours);
-            //minutes = Integer.parseInt(timeString.substring(1, 2));
             return null;
         }
         else {
@@ -36,21 +45,43 @@ public class FileConverter {
         return calendar.getTime();
     }
 
-    private void prepareWriting() {
-        try {
-            writer = new BufferedWriter(new FileWriter("output.csv"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void closeWriters() {
+        Iterator<BufferedWriter> writersIterator = writers.values().iterator();
+        while(writersIterator.hasNext()) {
+            try {
+                writersIterator.next().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            writersIterator.remove();
         }
     }
 
 
-    private void writeToFile(int flightNumber, String from, String to, Date depTime, Date arrTime, float cost, int numberOfFreePlaces) {
+    private BufferedWriter getWriter(String airline) {
+        if (writers.containsKey(airline)) {
+            return writers.get(airline);
+        } else {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(String.format("%s/%s.csv", outputPath, airline)));
+                writers.put(airline, writer);
+                return writer;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private void writeToFile(int flightNumber, String from, String to, Date depTime, Date arrTime, float cost, int numberOfFreePlaces, String airline) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         String output = String.format("\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\"", flightNumber, from, to, dateFormat.format(depTime), dateFormat.format(arrTime), cost, numberOfFreePlaces);
         try {
-            writer.write(output);
-            writer.newLine();
+            BufferedWriter writer = this.getWriter(airline);
+            if(writer != null) {
+                writer.write(output);
+                writer.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -58,11 +89,11 @@ public class FileConverter {
 
     private void performReading() {
         try {
-            reader = new BufferedReader(new FileReader("2008.csv"));
+            reader = new BufferedReader(new FileReader(this.inputPath));
             String line;
-            prepareWriting();
-            for(int i = 0; (line = reader.readLine()) != null && i<100000; i++) {
+            for(int i = 0; (line = reader.readLine()) != null && (i<this.neededObjects || this.neededObjects == -1); i++) {
                 if(i == 0) continue;
+                //System.out.println(line);
                 String[] components = line.split(",");
                 Date departureDate = constructDate(components[0], components[1], components[2], components[5]);
                 Date arrivalDate = constructDate(components[0], components[1], components[2], components[7]);
@@ -71,6 +102,7 @@ public class FileConverter {
                 }
                 String from = components[16];
                 String to = components[17];
+                String airline = components[8];
                 int freePlaces = rand.nextInt(400);
                 float cost = Math.abs(rand.nextFloat())*1000;
                 String flightNumber = components[9];
@@ -80,9 +112,9 @@ public class FileConverter {
                     cal.add(Calendar.DATE, 1);
                     arrivalDate = cal.getTime();
                 }
-                writeToFile(Integer.parseInt(flightNumber), from, to, departureDate, arrivalDate, cost, freePlaces);
+                writeToFile(Integer.parseInt(flightNumber), from, to, departureDate, arrivalDate, cost, freePlaces, airline);
             }
-            writer.close();
+            closeWriters();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -91,7 +123,7 @@ public class FileConverter {
     }
 
     public static void main(String[] args) {
-        FileConverter conv = new FileConverter();
+        FileConverter conv = new FileConverter("2008.csv", "output", -1);
         conv.performReading();
     }
 }
