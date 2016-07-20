7eb288d3ce2eada115041b8246a84972a8a8bac0
Theo Wood <KorsarUTrue@gmail.com>
2016-07-20 22:35:37 +0300

    Repare parce

--- src/innopolis/project/e4/Main.java
+++ src/innopolis/project/e4/Main.java
@@ -1,8 +1,12 @@
 package innopolis.project.e4;
 
+import innopolis.project.e4.providers.DataProvider;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        DataProvider dpTest = new DataProvider("D:\\Учёба\\Innopolis SummerSchool\\InnoAviaSales\\output.csv");
     }
 }
--- src/innopolis/project/e4/providers/DataProvider.java
+++ src/innopolis/project/e4/providers/DataProvider.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.List;
 
@@ -28,56 +29,64 @@ public class DataProvider {
     private HashMap<String, Airport> airports;
     private HashSet<Flight> flights;
     private String extentionOfDataSourceFile = ".csv";
-    private String separator = ",";
+    private String separator = ";";
     private String border = "\"";
     private int numOfFieldsInCSV = 7;
+    private String datePattern = "dd/MM/yyyy HH:mm:ss";
 
     public  DataProvider(String path){
 
         users = new HashMap<>();
         fileSoursesPath = path;
         fileSource = new File(fileSoursesPath);
+        airports = new HashMap<>();
+        companyCSV = new LinkedList<>();
+        flights = new HashSet();
+        flightsByAirports = new HashMap<>();
+
 
         if(fileSource.isDirectory()){
             for (File f : fileSource.listFiles()) {
                 if(f.isDirectory()){
                     for(File flight_f : f.listFiles())
                     companyCSV.add(flight_f);
-                }
-                else if(!f.isDirectory() && f.getName().contains(extentionOfDataSourceFile))
+                }else if(!f.isDirectory() && f.getName().contains(extentionOfDataSourceFile))
                     usersFile = f;
             }
+        }else if(fileSource.getName().compareTo("output.csv") == 0) {
+            parseFlightsCSV(new File("D:\\Учёба\\Innopolis SummerSchool\\InnoAviaSales\\output.csv"));
+            //companyCSV.add(fileSource);
         }
-        parseFlightsCSV();
         parseUserCSV();
     }
 
-    private boolean parseFlightsCSV() {
+    private boolean parseFlightsCSV(File f) {
         BufferedReader bufReader;
-        for (File f : companyCSV) {
+        {
             try {
                 bufReader = new BufferedReader(new FileReader(f));
                 String readingLine = "";
                 while ((readingLine = bufReader.readLine()) != null) {
-                    String[] line = readingLine.split(separator);
-                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                    Date dateD = df.parse(line[1]);
-                    Date dateA = df.parse(line[2]);
-                    airports.put(line[5], new Airport(line[5]));
-                    airports.put(line[6], new Airport(line[6]));
+                    String[] line = readingLine.replaceAll("\"","").split(separator);
+
+                    DateFormat df = new SimpleDateFormat(datePattern);
+                    Date dateD = df.parse(line[3]);
+                    Date dateA = df.parse(line[4]);
+                    airports.put(line[1], new Airport(line[1]));
+                    airports.put(line[2], new Airport(line[2]));
                     Flight fTmp = new Flight(
                             Integer.parseInt(line[0]),
                             dateD,
                             dateA,
-                            Float.parseFloat(line[3]),
-                            Integer.parseInt(line[4]),
+                            Float.parseFloat(line[5]),
+                            Integer.parseInt(line[6]),
                             f.getName().substring(0, f.getName().length() - 4), // "-4" cause '.CSV' so, company name,
                                                                                 // like "CompanyName.CSV" after substring
                                                                                 // will be like "CompanyName"
-                            airports.get(line[5]),
-                            airports.get(line[6]));
+                            airports.get(line[1]),
+                            airports.get(line[2]));
                     flights.add(fTmp);
-                    if(flightsByAirports.containsKey(airports.get(fTmp.getFrom()))){
+                    if(null != airports.get(fTmp) && flightsByAirports.containsKey(airports.get(fTmp.getFrom()))){
                         if(flightsByAirports.get(fTmp.getFrom()).containsKey(fTmp.getTo())){
                             flightsByAirports.get(fTmp.getFrom()).get(fTmp.getTo()).add(fTmp);
                         }else{
@@ -85,7 +94,9 @@ public class DataProvider {
                             flightsByAirports.get(fTmp.getFrom()).get(fTmp.getTo()).add(fTmp);
                         }
                     }else{
-                        flightsByAirports.put(fTmp.getFrom(), new HashMap<Airport, HashSet<Flight>>());
+                        flightsByAirports.put(
+                                fTmp.getFrom(),
+                                new HashMap<Airport, HashSet<Flight>>());
                         flightsByAirports.get(fTmp.getFrom()).put(fTmp.getTo(),new HashSet<Flight>());
                         flightsByAirports.get(fTmp.getFrom()).get(fTmp.getTo()).add(fTmp);
                     }
@@ -122,13 +133,14 @@ public class DataProvider {
 
     private void parseUserCSV(){
         BufferedReader bufReader;
+        if(usersFile == null) return;
         File f = usersFile;
             try {
                 bufReader = new BufferedReader(new FileReader(f));
                 String readingLine = "";
                 while ((readingLine = bufReader.readLine()) != null) {
                     String[] line = readingLine.split(separator);
-                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    DateFormat df = new SimpleDateFormat(datePattern);
                     Date dateD = df.parse(line[4]);
                     users.put(Integer.parseInt(line[0]), new User(Integer.parseInt(line[0]), line[1] + " " + line[2], line[3], dateD));
                 }
